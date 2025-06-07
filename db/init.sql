403d3d99bf025698cf89dbee90194dfb6431cc60
santino <santinopiantini@gmail.com>
2025-06-06 21:45:20 -0300

    changed init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,11 @@
 -- CREATE DATABASE IF NOT EXISTS peya
-SELECT 'CREATE DATABASE transactionDB'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'transactionDB')\gexec
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE transactions (
+                        transaction_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                        payment_id VARCHAR(50) NOT NULL,
+                        user_id VARCHAR(100) NOT NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
